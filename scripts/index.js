ea99d18d461aa06624bc70a43b482999bbf5dd9a
Lina Mykhailyk <mykhailyk.anhelina@gmail.com>
2025-01-20 20:19:39 +0200

    Add like and delete buttons functionality

--- scripts/index.js
+++ scripts/index.js
@@ -25,21 +25,22 @@ const initialCards = [
   },
 ];
 
-//Variables
+// Variables edit
 
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector("#edit-profile-modal");
 const profileModalCloseButton = profileEditModal.querySelector(
   ".modal__close-button"
 );
+
+// Variables add
 const addCardButton = document.querySelector(".profile__add-button");
 const addCardModal = document.querySelector("#add-card-modal");
 const addCardModalCloseButton = addCardModal.querySelector(
   ".modal__close-button"
 );
 
-const profileName = document.querySelector(".profile__title");
-const profileDescription = document.querySelector(".profile__description");
+// Variables form
 const profileNameInput = document.querySelector("#modal-name-input");
 const profileDescriptionInput = document.querySelector(
   "#modal-description-input"
@@ -48,6 +49,10 @@ const cardTitleInput = document.querySelector("#modal-title-input");
 const cardLinkInput = document.querySelector("#modal-link-input");
 const profileEditForm = profileEditModal.querySelector(".modal__form");
 const addCardForm = addCardModal.querySelector(".modal__form");
+
+// Other variables
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 const cardListElement = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
@@ -66,6 +71,15 @@ function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageElement = cardElement.querySelector(".card__image");
   const cardTitleElement = cardElement.querySelector(".card__title");
+  const cardLikeButton = cardElement.querySelector(".card__like-button");
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+  cardLikeButton.addEventListener("click", () => {
+    cardLikeButton.classList.toggle("card__like-button_active");
+  });
+  cardDeleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
   cardImageElement.src = cardData.link;
   cardImageElement.alt = cardData.name;
   cardTitleElement.textContent = cardData.name;
@@ -95,17 +109,21 @@ function handleAddCardSubmit(evt) {
 // Event listeners
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
 profileEditButton.addEventListener("click", () => {
   profileNameInput.value = profileName.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
   openPopup(profileEditModal);
 });
+
 profileModalCloseButton.addEventListener("click", () =>
   closePopup(profileEditModal)
 );
 
 addCardForm.addEventListener("submit", handleAddCardSubmit);
+
 addCardButton.addEventListener("click", () => openPopup(addCardModal));
+
 addCardModalCloseButton.addEventListener("click", () =>
   closePopup(addCardModal)
 );
